534d0a80c8e6e14f9acde81ff7bb352880884d1a
Guillaume Taquet Gasperini <perso@aerion.me>
2020-04-08 22:46:10 +0200

    Add support for Sqlite Exceptions
    Note that SQLite does not impose any length restrictions [0] so the
    MaxLength test doesn't make sense for this provider. Limiting to 15
    chars won't throw an exception (other than SQLITE_TOOBIG). Thus, the
    test has not been implemented.
    
    [0]: https://www.sqlite.org/datatype3.html

---
+++ EntityFramework.Exceptions.Tests/SqliteTests.cs
@@ -0,0 +1,60 @@
+﻿using EntityFramework.Exceptions.Common;
+using EntityFramework.Exceptions.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using Xunit;
+
+namespace EntityFramework.Exceptions.Tests
+{
+    public class SqliteTests : IClassFixture<SqliteDemoContextFixture>, IDisposable
+    {
+        private readonly DemoContextFixture fixture;
+
+        public SqliteTests(SqliteDemoContextFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void UniqueColumnViolationThrowsUniqueConstraintException()
+        {
+            fixture.Context.Products.Add(new Product { Name = "GD" });
+            fixture.Context.Products.Add(new Product { Name = "GD" });
+
+            Assert.Throws<UniqueConstraintException>(() => fixture.Context.SaveChanges());
+        }
+
+        [Fact]
+        public void RequiredColumnViolationThrowsCannotInsertNullException()
+        {
+            fixture.Context.Products.Add(new Product());
+
+            Assert.Throws<CannotInsertNullException>(() => fixture.Context.SaveChanges());
+        }
+
+        [Fact]
+        public void ReferenceViolationThrowsReferenceConstraintException()
+        {
+            fixture.Context.ProductSales.Add(new ProductSale { Price = 3.14m});
+
+            Assert.Throws<ReferenceConstraintException>(() => fixture.Context.SaveChanges());
+        }
+
+        public void Dispose()
+        {
+            foreach (var entityEntry in fixture.Context.ChangeTracker.Entries())
+            {
+                entityEntry.State = EntityState.Detached;
+            }
+        }
+    }
+
+    public class SqliteDemoContextFixture : DemoContextFixture
+    {
+        protected override DbContextOptionsBuilder<DemoContext> BuildOptions(DbContextOptionsBuilder<DemoContext> builder, IConfigurationRoot configuration)
+        {
+            return builder.UseSqlite(configuration.GetConnectionString("Sqlite")).UseExceptionProcessor();
+        }
+    }
+}
