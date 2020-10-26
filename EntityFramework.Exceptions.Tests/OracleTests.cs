913c7e11f7970cfb9c8ed790161744a48dc2f893
Arno Noordover <anoordover@users.noreply.github.com>
2020-10-26 15:37:53 +0100

    Added Unittest

---
+++ EntityFramework.Exceptions.Tests/OracleTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using EntityFramework.Exceptions.Oracle;
+using Xunit;
+
+namespace EntityFramework.Exceptions.Tests
+{
+    public class OracleTests : DatabaseTests, IClassFixture<OracleTestsontextFixture>
+    {
+        public OracleTests(OracleTestsontextFixture fixture) : base(fixture.Context)
+        {
+        }
+
+    }
+    
+    public class OracleTestsontextFixture : DemoContextFixture
+    {
+        protected override DbContextOptionsBuilder<DemoContext> BuildOptions(DbContextOptionsBuilder<DemoContext> builder, IConfigurationRoot configuration)
+        {
+            //var connectionString = configuration.GetConnectionString("Oracle");
+            var connectionString = "Data Source=brio.WORLD;Persist Security Info=True;User ID=bri;Password=bri;";
+            return builder.UseOracle(connectionString).UseExceptionProcessor();
+        }
+    }
+}
