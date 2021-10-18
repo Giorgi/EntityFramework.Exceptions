b684414db28199d9119d1401c772e18b0c106eaa
Giorgi <giorgi@giorgi.dev>
2021-10-18 13:03:29 +0400

    Check exception for null

--- EntityFramework.Exceptions.Common/ExceptionProcessorInterceptor.cs
+++ EntityFramework.Exceptions.Common/ExceptionProcessorInterceptor.cs
@@ -23,14 +23,16 @@ namespace EntityFramework.Exceptions.Common
         public override void SaveChangesFailed(DbContextErrorEventData eventData)
         {
             var dbUpdateException = eventData.Exception as DbUpdateException;
-            var providerException = eventData.Exception.GetBaseException() as T;
 
-            var error = GetDatabaseError(providerException);
-            
-            if (error != null && dbUpdateException != null)
+            if (eventData.Exception.GetBaseException() is T providerException)
             {
-                var exception = ExceptionFactory.Create(error.Value, dbUpdateException, dbUpdateException.Entries);
-                throw exception;
+                var error = GetDatabaseError(providerException);
+
+                if (error != null && dbUpdateException != null)
+                {
+                    var exception = ExceptionFactory.Create(error.Value, dbUpdateException, dbUpdateException.Entries);
+                    throw exception;
+                }
             }
 
             base.SaveChangesFailed(eventData);
@@ -40,14 +42,16 @@ namespace EntityFramework.Exceptions.Common
         public override Task SaveChangesFailedAsync(DbContextErrorEventData eventData, CancellationToken cancellationToken = new CancellationToken())
         {
             var dbUpdateException = eventData.Exception as DbUpdateException;
-            var providerException = eventData.Exception.GetBaseException() as T;
 
-            var error = GetDatabaseError(providerException);
-
-            if (error != null && dbUpdateException != null)
+            if (eventData.Exception.GetBaseException() is T providerException)
             {
-                var exception = ExceptionFactory.Create(error.Value, dbUpdateException, dbUpdateException.Entries);
-                throw exception;
+                var error = GetDatabaseError(providerException);
+
+                if (error != null && dbUpdateException != null)
+                {
+                    var exception = ExceptionFactory.Create(error.Value, dbUpdateException, dbUpdateException.Entries);
+                    throw exception;
+                }
             }
 
             return base.SaveChangesFailedAsync(eventData, cancellationToken);
