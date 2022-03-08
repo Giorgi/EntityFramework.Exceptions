a9efa60a9305c62f79f6f15d3e16c2d128d2fda0
Giorgi Dalakishvili <giorgi@giorgi.dev>
2022-03-08 22:13:31 +0400

    Reason for skipping test

--- EntityFramework.Exceptions.Tests/SqliteTests.cs
+++ EntityFramework.Exceptions.Tests/SqliteTests.cs
@@ -21,7 +21,7 @@ namespace EntityFramework.Exceptions.Tests
         [DllImport("e_sqlite3", CallingConvention = CallingConvention.Cdecl,EntryPoint = "sqlite3_limit")]
         private static extern int SetLimit(sqlite3 db, int id, int newVal);
 
-        [Fact(Skip = "")]
+        [Fact(Skip = "Skip temporarily")]
         public override async Task MaxLengthViolationThrowsMaxLengthExceededException()
         {
             Context.Database.OpenConnection();
