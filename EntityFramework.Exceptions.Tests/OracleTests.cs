d3ae270cc8c6fc3757f034114e066e51c0c7faa9
Giorgi Dalakishvili <giorgi@giorgi.dev>
2022-03-13 22:36:44 +0400

    Enable Oracle tests

--- EntityFramework.Exceptions.Tests/OracleTests.cs
+++ EntityFramework.Exceptions.Tests/OracleTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
-using EntityFramework.Exceptions.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using EntityFramework.Exceptions.Oracle;
@@ -8,18 +6,18 @@ using Xunit;
 
 namespace EntityFramework.Exceptions.Tests
 {
-    class OracleTests : DatabaseTests, IClassFixture<OracleTestContextFixture>
+    public class OracleTests : DatabaseTests, IClassFixture<OracleTestContextFixture>
     {
-        OracleTests(OracleTestContextFixture fixture) : base(fixture.Context)
+        public OracleTests(OracleTestContextFixture fixture) : base(fixture.Context)
         {
         }
     }
+
     public class OracleTestContextFixture : DemoContextFixture
     {
-        protected override DbContextOptionsBuilder<DemoContext> BuildOptions(
-            DbContextOptionsBuilder<DemoContext> builder, IConfigurationRoot configuration)
+        protected override DbContextOptionsBuilder<DemoContext> BuildOptions(DbContextOptionsBuilder<DemoContext> builder, IConfigurationRoot configuration)
         {
-            var connectionString = configuration.GetConnectionString("Oracle");
+            var connectionString = Environment.GetEnvironmentVariable("Oracle");
             return builder.UseOracle(connectionString).UseExceptionProcessor();
         }
     }
